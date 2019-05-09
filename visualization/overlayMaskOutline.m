f50cb73705a2d57b396dbcf8ba0e1ad98c4e45c2
pjl54 <pjl54@case.edu>
2019-05-09 10:48:28 -0400

    update overlayMask

--- visualization/overlayMaskOutline.m
+++ visualization/overlayMaskOutline.m
@@ -1,4 +1,4 @@
-function overlayMask(img,varargin)
+function overlayMaskOutline(img,varargin)
 
 imshow(img); hold on
 
@@ -8,10 +8,11 @@ colors = [0,1,0;0,0,1];
 
 for(k = 1:length(varargin))
     mask = varargin{k};
+    mask = imdilate(mask,strel('disk',3)) - mask;
 
     green = transImg .* reshape(colors(k,:),[1,1,3]);
     hold on
     h = imshow(green);
     hold off
-    set(h, 'AlphaData',mask .* .3)
+    set(h, 'AlphaData',mask .* .8)
 end
