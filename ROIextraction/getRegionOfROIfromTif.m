1cf6626daa0d662b6332125bec8c67f12d4aa965
pjl54 <40030915+pjl54@users.noreply.github.com>
2020-07-14 07:13:56 -0400

    update getRegionOfROIfromTif

--- ROIextraction/getRegionOfROIfromTif.m
+++ ROIextraction/getRegionOfROIfromTif.m
@@ -1,103 +1,35 @@
 % Region specified as [xStart width yStart height] or 'middle'
-function [ROI mask] = getRegionOfROIfromTif(imgPath,annotation,inputMag,outputMag,region,regionEdgeLength)
+function [ROI, region] = getRegionOfROIfromTif(imgPath,annotation,outputMPP,region,windowSize)
 
-% Matlab rotates some files (.scn, .tiff) when reading them in, need to undo this
-if(strcmp(imgPath(end-3:end),'.scn'))
-    rotation = 270;
-elseif(strcmp(imgPath(end-3:end),'.tiff'))
-    rotation = 180; % not actually implemented here
-else
-    rotation = 0;
-end
+minX = min(annotation.X);
+maxX = max(annotation.X);
 
-resFactor = inputMag/outputMag;
+minY = min(annotation.Y);
+maxY = max(annotation.Y);
 
-imgInfo = imfinfo(imgPath);
-imgWidths = extractfield(imgInfo,'Width');
-imgHeights = extractfield(imgInfo,'Height');
-[~,baseLayer] = max(imgWidths); % find which layer is highest magnification
-
-% Only consider layers larger than the desired output, downsize later if
-% needed.  "+ resFactor" is because every downsize can cut a few pixels off
-% due to rounding, wouldn't want to exclude an exact outputMag match.
-validLayers = find((imgWidths - imgWidths(baseLayer)/resFactor) + resFactor > 0);
-[~,vTarget] = min(abs(imgWidths(validLayers) - imgWidths(baseLayer)/resFactor));
-targetLayer = validLayers(vTarget);
-amountToReduceByLater = imgHeights(targetLayer) / (imgHeights(baseLayer) / resFactor); % if targetLayer doesn't match outputMag
-targetAnnoDown = resFactor/amountToReduceByLater; % how much to downsize annotation by when getting region, corresponds to magnification of targetLayer
-
-
-% Need to account for possible rotation
-switch rotation
-    case 0
-        
-        annStartX = round(min(annotation.Y/targetAnnoDown));
-        annEndX = round(max(annotation.Y/targetAnnoDown));
-        annStartY = round(min(annotation.X/targetAnnoDown));
-        annEndY = round(max(annotation.X/targetAnnoDown));
-        
-        
-    case 270
-        
-        annStartX = round(imgInfo(targetLayer).Height - max(annotation.X/targetAnnoDown));
-        annEndX = round(imgInfo(targetLayer).Height - min(annotation.X/targetAnnoDown));
-        annStartY = round(min(annotation.Y/targetAnnoDown));
-        annEndY = round(max(annotation.Y/targetAnnoDown));
-        
-        
-end
-
-imgHeight = annEndY - annStartY;
-imgWidth = annEndX - annStartX;
-
-if(ischar(region))
-    xStart = round(imgWidth/2)  + annStartX;
-    xEnd = xStart + regionEdgeLength(1);
+if(ischar(region))    
     
-    yStart = round(imgHeight/2) + annStartY;
-    yEnd = yStart + regionEdgeLength(2);
-
-%     yEnd = round(imgHeight/2);
-%     yStart = yEnd + regionEdgeLength(2);
-
+    xStart = round((minX+maxX)/2 - windowSize(1)/2);
+    width = windowSize(1);
+    yStart = round((minY+maxY)/2 - windowSize(2)/2);
+    height = windowSize(2);      
     
 else
-%     xStart = xStart + region(1);
-%     xEnd = xStart + region(1) + region(2);
     
+    xStart = region(1)+minX;
+    width = windowSize(1);
+    
+    yStart = region(2)+minY;
+    height = windowSize(2);
 end
 
-[annStartX annEndX annStartY annEndY]
-[xStart,xEnd,yStart,yEnd]
+newAnno.X = [xStart,xStart,xStart + width,xStart+ width];
+newAnno.Y = [yStart,yStart + height,yStart + height,yStart];
 
-rgb = imread(imgPath,'Index',targetLayer,'PixelRegion',{[xStart,xEnd],...
-    [yStart,yEnd]});
+region = [xStart - minX, width, yStart - minY, height];
 
-
-rgb = imrotate(rgb,rotation);
-rgb = imresize(rgb,1/amountToReduceByLater);
-
-% mask = poly2mask(round(annotation.X/resFactor - min(annotation.X/resFactor)),round(annotation.Y/resFactor - min(annotation.Y/resFactor)),...
-%     round(max(annotation.Y/resFactor)-min(annotation.Y/resFactor)),round(max(annotation.X/resFactor)-min(annotation.X/resFactor)));
-mask = ones(size(rgb,1),size(rgb,2));
-
-% zero pad or reduce mask to match size of image
-% rgb and mask can have different sizes due to rounding
-if(size(mask,1) > size(rgb,1))
-    mask = mask(1:size(rgb,1),:);
-elseif(size(mask,1) < size(rgb,1))
-    mask = padarray(mask,[size(rgb,1) - size(mask,1) 0],'post');
-end
-
-if(size(mask,2) > size(rgb,2))
-    mask = mask(:,1:size(rgb,2));
-elseif(size(mask,2) < size(rgb,2))
-    mask = padarray(mask,[0 size(rgb,2) - size(mask,2) 0],'post');
-end
-
-ROI = rgb;
-
-if(nargout == 1)
-    ROI = ROI .* uint8(repmat(mask,1,1,size(ROI,3)));
-    ROI = ROI + uint8(repmat(~mask,1,1,3))*255;
-end
+% if(nargout == 1)
+    ROI = getROIfromTif(imgPath,newAnno,outputMPP);
+% else
+%     [ROI, mask] = getROIfromTif(imgPath,newAnno,outputMPP);
+% end
